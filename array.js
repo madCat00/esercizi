bdb53945a72b7649b2c894416a3f79b7907cf8cc
madCat00 <francescoxx35@gmail.com>
2020-11-02 13:01:01 +0100

    esercizi con le stringhe

---
+++ array.js
@@ -0,0 +1,9 @@
+const stringhe = ['hello','animali','macchine','colori','alberi'];
+
+function string (stringa1){
+    return stringa1.toUpperCase();
+}
+const input = string(stringhe[2]);
+string(input);
+console.log(input);
+console.log(string(stringhe.length));
